4e5d758f87e27903c7fe9e28e8dc2ccacb706135
Florian Schober <f.schober@live.com>
2019-01-08 21:55:14 +0100

    new release 1.1.4

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -85,6 +85,6 @@ namespace bla
 {
     internal static class VersionNumberHack
     {
-        public const string VersionNumber = "1.1.3.0";
+        public const string VersionNumber = "1.1.4.0";
     }
 }
