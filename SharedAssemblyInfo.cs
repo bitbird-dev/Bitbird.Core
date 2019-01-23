aaaa4538063d16fc079c1b09d630d14aa4bbce1a
Florian Schober <f.schober@live.com>
2019-01-23 19:15:16 +0100

    updated nuget refs

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -85,6 +85,6 @@ namespace bla
 {
     internal static class VersionNumberHack
     {
-        public const string VersionNumber = "1.1.9.0";
+        public const string VersionNumber = "1.1.10.0";
     }
 }
