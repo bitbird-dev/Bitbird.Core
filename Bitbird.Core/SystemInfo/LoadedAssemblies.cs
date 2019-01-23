f53fd124e78d42eff20c44e31237619b83362cfc
Florian Schober <f.schober@live.com>
2019-01-23 21:02:01 +0100

    added systeminfo

---
+++ Bitbird.Core/SystemInfo/LoadedAssemblies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Bitbird.Core.SystemInfo
+{
+    public static class LoadedAssemblies
+    {
+        public static string ReportLoadedAssemblies()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Loaded assemblies:");
+            foreach (var loadedAssembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var assemblyName = loadedAssembly.GetName();
+                sb.AppendFormat("  {0}:v{1} ({2})", assemblyName.Name, assemblyName.Version, loadedAssembly.Location);
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Bitbird.Core/SystemInfo/SystemInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Bitbird.Core.SystemInfo
+{
+    public static class SystemInfo
+    {
+        public static string ReportPrimitiveSystemInfo()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Primitive Info:");
+
+            foreach (var propertyInfo in typeof(Environment).GetProperties(BindingFlags.Public | BindingFlags.Static))
+            {
+                if (!propertyInfo.CanRead)
+                    continue; 
+
+                try
+                {
+                    sb.AppendFormat("  {0}: {1}", propertyInfo.Name, propertyInfo.GetValue(null));
+                }
+                catch (Exception e)
+                {
+                    sb.AppendFormat("  {0}: EXCEPTION {2}({1})", propertyInfo.Name, e.GetType(), e.Message);
+                }
+                sb.AppendLine();
+            }
+
+            foreach (var fieldInfo in typeof(Environment).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                try
+                {
+                    sb.AppendFormat("  {0}: {1}", fieldInfo.Name, fieldInfo.GetValue(null));
+                }
+                catch (Exception e)
+                {
+                    sb.AppendFormat("  {0}: EXCEPTION {2}({1})", fieldInfo.Name, e.GetType(), e.Message);
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
