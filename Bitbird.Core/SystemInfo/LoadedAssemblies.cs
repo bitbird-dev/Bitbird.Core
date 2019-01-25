cec06600ac7e99afe9dc6bde1a9a04683c2266bb
Florian Schober <f.schober@live.com>
2019-01-25 15:31:05 +0100

    fixed bug in assembly reporting

--- Bitbird.Core/SystemInfo/LoadedAssemblies.cs
+++ Bitbird.Core/SystemInfo/LoadedAssemblies.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Text;
 
 namespace Bitbird.Core.SystemInfo
@@ -12,8 +13,48 @@ namespace Bitbird.Core.SystemInfo
             sb.AppendLine("Loaded assemblies:");
             foreach (var loadedAssembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                var assemblyName = loadedAssembly.GetName();
-                sb.AppendFormat("  {0}:v{1} ({2})", assemblyName.Name, assemblyName.Version, loadedAssembly.Location);
+                AssemblyName assemblyName;
+                try
+                {
+                    assemblyName = loadedAssembly.GetName();
+                }
+                catch (Exception e)
+                {
+                    sb.AppendFormat("  EXCEPTION {0}({1})", e.GetType(), e.Message);
+                    sb.AppendLine();
+                    continue;
+                }
+
+                string name;
+                string version;
+                string location;
+
+                try
+                {
+                    name = assemblyName.Name;
+                }
+                catch (Exception e)
+                {
+                    name = $"EXCEPTION {e.GetType()}({e.Message})";
+                }
+                try
+                {
+                    version = assemblyName.Version.ToString();
+                }
+                catch (Exception e)
+                {
+                    version = $"EXCEPTION {e.GetType()}({e.Message})";
+                }
+                try
+                {
+                    location = loadedAssembly.Location;
+                }
+                catch (Exception e)
+                {
+                    location = $"EXCEPTION {e.GetType()}({e.Message})";
+                }
+
+                sb.AppendFormat("  {0}:v{1} ({2})", name, version, location);
                 sb.AppendLine();
             }
 
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -85,6 +85,6 @@ namespace bla
 {
     internal static class VersionNumberHack
     {
-        public const string VersionNumber = "1.1.17.0"; 
+        public const string VersionNumber = "1.1.18.0"; 
     }
 }
