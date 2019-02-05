68905f7bf3c55868b70386ee2f59e42c60d20cb8
Florian Schober <f.schober@live.com>
2019-02-05 17:52:18 +0100

    release 1.1.27

--- Bitbird.Core.WebApi.Net/JsonApiOverridePrimaryType.cs
+++ Bitbird.Core.WebApi.Net/JsonApiOverridePrimaryType.cs
@@ -1,5 +1,16 @@
 ﻿namespace Bitbird.Core.WebApi.Net
 {
+    public class JsonApiOverridePrimaryType<T> : IJsonApiOverridePrimaryType
+    {
+        public object Data { get; }
+        public string Type { get; }
+
+        public JsonApiOverridePrimaryType(T data, string type)
+        {
+            Data = data;
+            Type = type;
+        }
+    }
     public class JsonApiOverridePrimaryType : IJsonApiOverridePrimaryType
     {
         public object Data { get; }
