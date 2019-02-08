fcf48c4ffd26113b16e54435bfdc87f421232e60
Florian Schober <f.schober@live.com>
2019-02-08 18:50:07 +0100

    release 1.1.29. JsonApiResourceMappingAttribute stores more information.

--- Bitbird.Core.WebApi.Net/JsonApiResourceMappingAttribute.cs
+++ Bitbird.Core.WebApi.Net/JsonApiResourceMappingAttribute.cs
@@ -2,15 +2,36 @@
 
 namespace Bitbird.Core.WebApi.Net
 {
-    /// <inheritdoc />
+    /// <summary>
+    /// Maps a resource to a model class.
+    /// Can be used to identify resources for a model class and vice-versa if a meta-data-structure is built.
+    /// Not all resources with this attribute have their own controller.
+    /// Some resources are only used during data-transfer (serialization and deserialization).
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class JsonApiResourceMappingAttribute : Attribute
     {
+        /// <summary>
+        /// The model type that should be associated with the resource class that is assigned this resource.
+        /// </summary>
         public readonly Type Type;
+        /// <summary>
+        /// Whether this resource is only used for data-transfer (serialization and deserialization), or has its own controller to be managed with.
+        /// </summary>
+        public readonly bool DataTransferOnly;
 
-        public JsonApiResourceMappingAttribute(Type type)
+        /// <summary>
+        /// Maps a resource to a model class.
+        /// Can be used to identify resources for a model class and vice-versa if a meta-data-structure is built.
+        /// Not all resources with this attribute have their own controller.
+        /// Some resources are only used during data-transfer (serialization and deserialization).
+        /// </summary>
+        /// <param name="type">The model type that should be associated with the resource class that is assigned this resource.</param>
+        /// <param name="dataTransferOnly">Whether this resource is only used for data-transfer (serialization and deserialization), or has its own controller to be managed with.</param>
+        public JsonApiResourceMappingAttribute(Type type, bool dataTransferOnly)
         {
             Type = type;
+            DataTransferOnly = dataTransferOnly;
         }
     }
 }
