215dfe48298de1ed0695e9463013bc245785a8c2
Florian Schober <f.schober@live.com>
2019-02-11 17:34:09 +0100

    documentation: first try

---
+++ Bitbird.Core.Tools.Doc.DotNet/tools/build.ps1
@@ -0,0 +1,30 @@
+param(
+	[string] $ConfigFile,
+	[string] $OutputArchieve = "Documentation.zip"
+	[string] $TempDir = "bin/docfx_build"
+)
+
+[string] $templateBuild = Get-Content ([System.IO.Path]::Combine($PSScriptRoot, "..", "resources", "build.json.tmpl")) -Raw;
+
+$config = Get-Content -Raw -Path $ConfigFile | ConvertFrom-Json;
+
+Get-ChildItem -Path $TempDir -File | Remove-Item;
+Get-ChildItem -Path $TempDir -File | Remove-Item -Recurse;
+
+[string] $build = $templateBuild;
+$build = $build.Replace("{title}", $config.title);
+$build = $build.Replace("{footer}", $config.footer.Replace("{year}",[System.DateTime]::Now.Year.ToString()));
+
+[string] $buildConfigPath = [System.IO.Path]::Combine((Get-Location), "docfx.build.json");
+$build | Out-File $buildConfigPath -Encoding utf8 -NoNewline;
+
+[string] $templatePath = [System.IO.Path]::Combine((Get-Location), "DocFxTemplate");
+Copy-Item ([System.IO.Path]::Combine($PSScriptRoot, "..", "resources", "DocFxTemplate", "*")) -Destination $templatePath -Recurse -Force;
+
+$args = @($buildConfigPath);
+& "docfx.exe" $args;
+
+Remove-Item $buildConfigPath;
+Remove-Item $templatePath -Recurse;
+
+Compress-Archive -Path ([System.IO.Path]::Combine($TempDir, "*")) -DestinationPath $OutputArchieve;
---
+++ Bitbird.Core.Tools.Doc.DotNet/tools/export.ps1
@@ -0,0 +1,44 @@
+param(
+	[string] $ConfigFile,
+	[string] $OutputArchieve = "Project_Documentation.zip"
+	[string] $TempDir = "bin/docfx"
+)
+
+[string] $templateMetaData = Get-Content ([System.IO.Path]::Combine($PSScriptRoot, "..", "resources", "export.metadata.json.tmpl")) -Raw;
+[string] $templateMetaDataItem = Get-Content ([System.IO.Path]::Combine($PSScriptRoot, "..", "resources", "export.metadata.item.json.tmpl")) -Raw;
+
+$config = Get-Content -Raw -Path $ConfigFile | ConvertFrom-Json;
+
+Get-ChildItem -Path $TempDir -File | Remove-Item;
+Get-ChildItem -Path $TempDir -File | Remove-Item -Recurse;
+
+[string] $items = "";
+foreach ($api in $config.api) {
+	[string] $item = $templateMetaDataItem;
+	$item = $item.Replace("{csproj}", $api.csproj);
+	$item = $item.Replace("{output}", [System.IO.Path]::Combine($TempDir, $api.csproj));
+	$items = $items + $item;
+}
+
+[string] $metaData = $templateMetaData;
+$metaData = $metaData.Replace("{entries}", $items);
+
+[string] $metaDataConfigPath = [System.IO.Path]::Combine((Get-Location), "docfx.metadata.json");
+$metaData | Out-File $metaDataConfigPath -Encoding utf8 -NoNewline;
+
+[string] $filterPath = [System.IO.Path]::Combine((Get-Location), "filterConfig.yml");
+Copy-Item ([System.IO.Path]::Combine($PSScriptRoot, "..", "resources", "filterConfig.yml")) -Destination $filterPath -Force;
+
+$args = @($metaDataConfigPath);
+& "docfx.exe" $args;
+
+Remove-Item $metaDataConfigPath;
+Remove-Item $filterPath;
+
+foreach ($content in $config.content) {
+	[string] $dst = [System.IO.Path]::Combine((Get-Location), $content.dst);
+    [System.IO.Directory]::CreateDirectory($dst);
+	Copy-Item ([System.IO.Path]::Combine((Get-Location), $content.src, "*")) -Destination $dst -Recurse -Force;
+}
+
+Compress-Archive -Path ([System.IO.Path]::Combine($TempDir, "*")) -DestinationPath $OutputArchieve;
