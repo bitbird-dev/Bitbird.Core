1e25ab2a0b918f398e3b9428d22c448ba1ce6eb1
Florian Schober <f.schober@live.com>
2019-04-16 00:57:59 +0200

    new release 1.1.53

---
+++ Bitbird.Core/ApiErrors/ApiUserIsInActiveError.cs
@@ -0,0 +1,10 @@
+﻿namespace Bitbird.Core
+{
+    public class ApiUserIsInActiveError : ApiError
+    {
+        public ApiUserIsInActiveError()
+            : base(ApiErrorType.UserIsLocked, "User is inactive", "The user is currently inactive. Contact your administrator.")
+        {
+        }
+    }
+}
