32ee0ead828b809807ffccb04e2235623297b094
Florian Schober <f.schober@live.com>
2019-06-27 17:12:12 +0200

    moved nuget package generation to post-build, added script to publish packages locally, updated to 1.2.37

---
+++ Publish-To-Local.ps1
@@ -0,0 +1,19 @@
+param(
+	[string] $Dir = $null
+)
+
+if ([string]::IsNullOrWhitespace($Dir)) {
+	$Dir = "{0}\source\local nuget" -f $env:USERPROFILE;
+}
+
+Write-Host ("Publishing to local nuget dir: {0}" -f $Dir);
+
+[string[]] $packages = Get-ChildItem *.nupkg -Recurse | ?{ !$_.FullName.Contains("packages\") } | %{ $_.FullName };
+
+foreach ($package in $packages) {
+	$newPath = [System.IO.Path]::Combine($Dir, [System.IO.Path]::GetFilename($package));
+	if (![System.IO.File]::Exists($newPath)) {
+		Write-Host ("Publishing {0}" -f ([System.IO.Path]::GetFilename($package)));
+		Copy-Item $package $Dir;
+	}
+}
