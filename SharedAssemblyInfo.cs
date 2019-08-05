8edcbbae1a23dd274fa7b0870e365944743327da
Florian Schober <f.schober@live.com>
2019-08-05 19:54:06 +0200

    new version 1.2.66

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,6 +21,6 @@ namespace Bitbird.Core
 {
     internal static class SharedAssemblyVersion
     {
-        public const string VersionNumberString = "1.2.65.0"; 
+        public const string VersionNumberString = "1.2.66.0"; 
     }
 }
